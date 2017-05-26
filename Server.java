8e0a6f2d83ebbfc390e0f1cbf14e271ff59d7c86
bfeelin <brianphelan60@gmail.com>
2017-05-25 21:18:18 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -7,12 +7,12 @@ import java.net.Socket;
 
 public class Server {
 
-	public static void main(String[] args) throws IOException 
+	public static void main(String[] args) throws IOException, InterruptedException 
 	{
 		run();
 	}
 	
-	public static void run() throws IOException
+	public static void run() throws IOException, InterruptedException
 	{
 		try
 		{
@@ -20,22 +20,23 @@ public class Server {
 				ServerSocket serverSocket = new ServerSocket(9998);
 			    System.out.println("Server socket created");
 			    Socket clientSocket = serverSocket.accept();
-			    PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
-			    BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			    PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);							//to write to Client
+			    BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));	//to read from Client
 			    
 			    String inputLine = null, outputLine = null;
 	            while ((inputLine = input.readLine()) != null) 
 	            {	
-	            	Process p;
-	                try {
-	                    p = Runtime.getRuntime().exec(inputLine);
-	                    BufferedReader br = new BufferedReader(
-	                        new InputStreamReader(p.getInputStream()));
-	                    p.waitFor();
-	                    System.out.println ("exit: " + p.exitValue());
-	                    p.destroy();
-	                } catch (Exception e) {}
-	                output.println(outputLine);
+	            	System.out.println("Running " + inputLine);           	
+	            	Process p = Runtime.getRuntime().exec(inputLine);
+	            	BufferedReader readOutput = new BufferedReader(new InputStreamReader(p.getInputStream()));	
+	            	
+	            	while((outputLine = readOutput.readLine()) != null)	
+	            		output.println(outputLine);
+	            	
+	            	p.waitFor();
+	            	System.out.println("Finished executing " + inputLine);
+	            	p.destroy();
+	            	
 	            }
 		}
 		catch (IOException e) 
