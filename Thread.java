61535ec2277d765ceeb07fcccb0e911de6fa8d49
bfeelin <brianphelan60@gmail.com>
2017-05-30 21:39:51 -0400

    Update Thread.java

--- Thread.java
+++ Thread.java
@@ -2,47 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
-public class Server {
-	static ArrayList<Thread> threads = new ArrayList<Thread>();
-	public static void main(String[] args) throws IOException, InterruptedException 
+public class Thread 
+{
+	public Socket socket;
+	public float respTime;
+	Thread(Socket clientSocket)
 	{
-		run();
+		this.socket = clientSocket;
 	}
-	
-	public static void run() throws IOException, InterruptedException
-	{
-		try
-		{
-			    @SuppressWarnings("resource")
-				ServerSocket serverSocket = new ServerSocket(9990);
-			    System.out.println("Server socket created");
-			    Socket clientSocket = serverSocket.accept();
-			    PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);							//to write to Client
-			    String inputLine = null;
-			    int i = 0;
-			    int noClients = 10;
 
-			    	for(i = 0; i < noClients; i++) 
-	            	{
-	                    Thread newThread = new Thread(clientSocket);
-	                    threads.add(newThread);      
-	            	}
-	            	 BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));	//to read from Client
-		            while ((inputLine = input.readLine()) != null) 									//while there are more commands
-		            {	    	
-		            	for(i = 0; i < noClients; i++)
-		            		threads.get(i).start(inputLine);  
-		            	output.println("end");
-		            }
-		}
-		catch (IOException e) 
-		{
-            System.out.println("Exception caught when trying to listen on port or listening for a connection");
-            System.out.println(e.getMessage());
-        }
+	void start(String cmd) throws IOException, InterruptedException
+	{
+		System.out.println("Running " + cmd);	
+		run(cmd);
+		System.out.println("Finished executing " + cmd + "Response time : " + this.respTime);
+	}
+	void run(String cmd) throws IOException, InterruptedException
+	{
+		String outputLine = null;
+		float startTime = System.currentTimeMillis();
+		Process p = Runtime.getRuntime().exec(cmd);							//run the command
+    	BufferedReader readOutput = new BufferedReader(new InputStreamReader(p.getInputStream()));	 //to read what is printed by the command
+    	PrintWriter output = new PrintWriter(this.socket.getOutputStream(), true);							//to write to Client
+    	while((outputLine = readOutput.readLine()) != null)							//while there is more output
+    		output.println(outputLine);												//send it to the client
+    	float endTime = System.currentTimeMillis();
+	    this.respTime = endTime - startTime;
+    														//send "end" to indicate end of output
+    	p.waitFor();
+    	p.destroy();
+	    
 	}
 }
+
